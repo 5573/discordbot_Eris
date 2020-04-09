1e6e6a0c4d94ff3a1c56467ca690512cc5d9c13a
557 <maimaiteru@me.com>
2020-04-10 00:01:27 +0900

    [fix] add host

--- service/database.js
+++ service/database.js
@@ -2,6 +2,7 @@ const { Sequelize } = require('sequelize')
 require('dotenv').config()
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+  host: 'us-cdbr-iron-east-01.cleardb.net'
   dialect: 'mysql',
   timezone:'+09:00'
 });
