579290cd0c9867045727f416ee9e42e600d2c7f6
557 <maimaiteru@me.com>
2020-05-03 10:27:24 +0900

    [fix] channelSet

--- index.js
+++ index.js
@@ -13,6 +13,9 @@ bot.on("ready", () => {
 });
 
 bot.on("voiceChannelJoin", async (member, newChannel) => {
+  // if(newChannel.guild.id === )
+  console.log(newChannel.guild.id, newChannel.guild.name )
+  if(newChannel.guild.id === process.env.GUILD_ID) {
     bot.createMessage(process.env.CHANNEL1, time.formatTime() + " に " + (member.nick ? member.nick : member.username) + " が " + newChannel.name + " に入室しました。");
     try {
       let newStatus = await UserStatus.create({
@@ -24,34 +27,42 @@ bot.on("voiceChannelJoin", async (member, newChannel) => {
     } catch(ex) {
       console.log(ex);
     }
+  }
+
 });
 
 bot.on("voiceChannelLeave", async (member, oldChannel) => {
-        bot.createMessage(process.env.CHANNEL1, time.formatTime() + " に " + (member.nick ? member.nick : member.username)  + " が " + oldChannel.name + " から退出しました。");
-        try {
-          let newStatus = await UserStatus.create({
-            user_id: member.id,
-            name: member.username,
-            guild_id: member.guild.id,
-            io: 0
-          });
-        } catch(ex) {
-          console.log(ex);
-        }
+  console.log(oldChannel.guild.id, oldChannel.guild.name )
+  if(oldChannel.guild.id === process.env.GUILD_ID) {
+    bot.createMessage(process.env.CHANNEL1, time.formatTime() + " に " + (member.nick ? member.nick : member.username)  + " が " + oldChannel.name + " から退出しました。");
+    try {
+      let newStatus = await UserStatus.create({
+        user_id: member.id,
+        name: member.username,
+        guild_id: member.guild.id,
+        io: 0
+      });
+    } catch(ex) {
+      console.log(ex);
+    }
+  }
 });
 
 bot.on("voiceChannelSwitch", async (member, newChannel, oldChannel) => {
-        bot.createMessage(process.env.CHANNEL1, time.formatTime() + " に " + (member.nick ? member.nick : member.username) + " が " + oldChannel.name + " から " + newChannel.name + " に移動しました。");
-        try {
-          let newStatus = await UserStatus.create({
-            user_id: member.id,
-            name: member.username,
-            guild_id: member.guild.id,
-            io: 3
-          });
-        } catch(ex) {
-          console.log(ex);
-        }
+  console.log(newChannel.guild.id, newChannel.guild.name )
+  if(newChannel.guild.id === process.env.GUILD_ID) {
+    bot.createMessage(process.env.CHANNEL1, time.formatTime() + " に " + (member.nick ? member.nick : member.username) + " が " + oldChannel.name + " から " + newChannel.name + " に移動しました。");
+    try {
+      let newStatus = await UserStatus.create({
+        user_id: member.id,
+        name: member.username,
+        guild_id: member.guild.id,
+        io: 3
+      });
+    } catch(ex) {
+      console.log(ex);
+    }
+  }
 });
 
 
